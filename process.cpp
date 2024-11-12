1cf3c19d0c9e1ff6dfa87df604507aba393c681f
guobing <635423240@qq.com>
2024-11-12 18:21:03 +0800

    version 0.0.2

--- process.cpp
+++ process.cpp
@@ -7,9 +7,5 @@ void Process::planProcess()
 {
     cout << "this is planning process" << endl;
     my_map.map_Info();
-<<<<<<< HEAD
     cout << "planning success" << endl;
-=======
-    cout << "planning success！" << endl;
->>>>>>> 3b07506f62d75f6e18f9f3389c15dd772b4f7164
 }
