deac14274a1c17ad19db3d31e175ee9fb5300051
guobing <635423240@qq.com>
2024-11-12 17:16:21 +0800

    version 0.0.1

---
+++ process.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "process.h"
+
+using namespace std;
+
+void Process::planProcess()
+{
+    cout << "this is planning process" << endl;
+    my_map.map_Info();
+    cout << "planning success！" << endl;
+}
